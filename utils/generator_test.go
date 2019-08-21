31fdb10b15ea8bdc74837bd4951dae6fb629d595
nagatax <4foview@gmail.com>
2019-08-20 19:12:10 -0700

    Change import statements order

--- utils/generator_test.go
+++ utils/generator_test.go
@@ -1,8 +1,9 @@
 package utils
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test整数が取得できること(t *testing.T) {
--- utils/operation_test.go
+++ utils/operation_test.go
@@ -1,8 +1,9 @@
 package utils
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test105を10で割った商が10で余りが5であること(t *testing.T) {
